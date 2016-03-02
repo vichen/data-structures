7197b0082321b408ef97e01f7a700155c7a58745
Vivian Chen <vivian.vc.chen@gmail.com>
2016-03-02 14:29:44 -0800

    modify queue and stack

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,22 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var items = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[items] = value;
+    items++;
   };
 
   someInstance.dequeue = function() {
+    items--;
+    return storage[0];
   };
 
   someInstance.size = function() {
+    return Math.max(0, items);
   };
 
   return someInstance;
