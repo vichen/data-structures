b85b3af2c0ae015b592c663eb171450a9f466c08
Vivian Chen <vivian.vc.chen@gmail.com>
2016-03-02 22:23:37 -0800

    complete functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,51 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var items = 0;
-<<<<<<< HEAD
-
-  //array variable
-
-  //use enqueue to push into newArray
-    //return array 
-=======
->>>>>>> 7197b0082321b408ef97e01f7a700155c7a58745
+  var oldestIndex = 0;
+  var newestIndex = 0;
 
 
   // Implement the methods below
   // Load Queue
   someInstance.enqueue = function(value) {
-    storage[items] = value;
-    items++;
+    storage[oldestIndex] = value;
+    oldestIndex++;
   };
 
   // Unload Queue 
   someInstance.dequeue = function() {
-    items--;
-<<<<<<< HEAD
-    var deq = storage [0];
-    return deq;
-    delete storage[0];
-    for (var prop in storage) {
-      var temp = prop - 1;
-      prop = temp;
-    }
-    
-    // check if storage[0] === empty
-    
-  
-    // for (var prop in storage) {
-    //   var temp = storage[prop];
-    //   storage[prop] = storage[(prop + 1)];
-    // }
-=======
-    return storage[0];
->>>>>>> 7197b0082321b408ef97e01f7a700155c7a58745
+    var temp = storage[newestIndex];
+    newestIndex++;
+    return temp;
+
   };
 
 
   someInstance.size = function() {
-    return Math.max(0, items);
+    return Math.max(0, oldestIndex - newestIndex);
   };
 
   return someInstance;
