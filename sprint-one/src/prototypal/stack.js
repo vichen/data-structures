a3d743d5971b31399571476c0a8f14637aa02ac4
Vivian Chen <vivian.vc.chen@gmail.com>
2016-03-02 23:30:58 -0800

    modify prototypal stack

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
+'src/functional-shared/stack.js'
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(Stack.stuff);
+  newStack.index = 0;
+  newStack.storage = {};
+
+  return newStack;
 };
 
-var stackMethods = {};
+Stack.stuff = {};
+
+Stack.stuff.push = function(value) {
+	this.storage[this.index++];
+};
+
+Stack.stuff.pop = function() {
+	if (this.index) {
+		this.index--;
+	}
+	var result = this.storage[this.index];
+
+	delete this.storage[this.index];
+
+	return result;
+};
+
+Stack.stuff.size = function() {
+	return this.index;
+};
 
 
