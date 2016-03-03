ab57a553644f94cc5010696cce2d8b90a46c05fb
vichen <vivian.vc.chen@gmail.com>
2016-03-03 09:48:38 -0800

    complete pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,22 +1,26 @@
 var Queue = function() {
   this.storage = {};
-  this.oldestIndex = 0;
-  this.newestIndex = 0;
+  this.items = 0;
+
 };
 
 Queue.prototype.enqueue = function(value) {
-  this.oldestIndex++;
-  this.storage[this.oldestIndex] = value;
+  this.storage[this.items] = value;
+  this.items++;
 };
 
 Queue.prototype.dequeue = function() {
-  var temp = this.storage[this.newestIndex];
-  this.newestIndex++;
-  return temp;
+  var first = this.storage[0];
+  this.items && this.items--;
+  for (var i = 0; i < Object.keys(this.storage).length; i++) {
+    this.storage[i] = this.storage[i + 1];
+  }
+  delete this.storage[this.items];
+  return first;
 };
 
 Queue.prototype.size = function() {
-  return Math.max(0, this.oldestIndex - this.newestIndex);
+  return Math.max(0, this.items);
 };
 
 var smallQueue = new Queue('a', 'b', 'c');
