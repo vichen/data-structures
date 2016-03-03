f175f47500b4af5d8f65ceca8a885c2fd094e68c
Vivian Chen <vivian.vc.chen@gmail.com>
2016-03-03 13:47:25 -0800

    modify linked lists

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail && list.head === null) {
+      list.head = Node(value);
+    }
+    list.tail = Node(value);
   };
 
   list.removeHead = function() {
+    
   };
 
   list.contains = function(target) {
+    var temp = list.head;
+    var index = 0;
+    while (temp.value !== target) {
+      temp = temp.next;
+      index++;
+      if (temp === null) {
+        return -1;
+      }
+      return index;
+    }
+/* 
+temp = head, index = 0
+while (temp.data != input)
+  temp = temp.next , index++
+  if temp == null
+    return -1
+return index;
+ */
   };
 
   return list;
