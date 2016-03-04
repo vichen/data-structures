bb0fc067389e675bf013f188cb1c3164b63fe757
vichen <vivian.vc.chen@gmail.com>
2016-03-04 12:27:23 -0800

    modify set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,15 +1,26 @@
+// An .add() method, takes any string and adds it to the set
+// A .contains() method, takes any string and returns a boolean reflecting whether it can be found in the set
+// A .remove() method, takes any string and removes it from the set, if present
+
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = {};
+  set.storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage.push[item];
+  
 };
 
 setPrototype.contains = function(item) {
+  for (var i = 0; i < this.storage.length; i++) {
+    if (this.storage[i] === item) {
+      return true;
+    }
+  }
 };
 
 setPrototype.remove = function(item) {
