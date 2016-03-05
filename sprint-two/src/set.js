2b8a92d93e497a7cd8229410f55a6cb3aa24daaa
Vivian Chen <vivian.vc.chen@gmail.com>
2016-03-04 20:35:58 -0800

    finish set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -2,29 +2,25 @@
 // A .contains() method, takes any string and returns a boolean reflecting whether it can be found in the set
 // A .remove() method, takes any string and removes it from the set, if present
 
+
 var Set = function() {
   var set = Object.create(setPrototype);
-  set.storage = storage;
+  set.storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
-  this.storage = [];
-  this.storage = this.storage.push[item];
-  
+  this.storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
-  for (var i = 0; i < this.storage.length; i++) {
-    if (this.storage[i] === item) {
-      return true;
-    }
-  }
+  return item in this.storage;
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[item];
 };
 
 /*
